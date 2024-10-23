7be1609689474870592a3b3cbacb9a39a45aadef
Abhimanyu2510 <145564651+Abhimanyu2510@users.noreply.github.com>
2024-10-23 20:03:27 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,15 @@ from setuptools import setup, find_packages
 setup(
     name="clustering-app",
     version="0.1",
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[
-        "numpy",
-        "pandas",
-        "scikit-learn",
-        "streamlit",
-        "pytest",
-        "joblib"
+        "numpy>=1.24.3",
+        "pandas>=2.0.3",
+        "scikit-learn>=1.3.0",
+        "streamlit>=1.25.0",
+        "pytest>=7.4.0",
+        "joblib>=1.3.1",
     ],
+    python_requires=">=3.9",
 )
